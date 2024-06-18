fa0668d543953563f047875586de20d593675a54
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-06-18 21:35:50 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useState } from "react";
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
+  let[count,setData]=useState(0)
+ 
+     
+       
+  function updateData() {
+    setData(count+1)
+  }
+  return(
+<div className="App">
+  
+  <p>button cliked {count} times</p>
+  <button onClick={updateData}>click me</button>
     </div>
   )
 }
